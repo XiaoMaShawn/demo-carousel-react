e5d501cfcb7f30e9621c330fb5f2abff59da10ba
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-04-07 21:31:15 -0400

    mouse hover/leave to pause/resume slider

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CarouselItem from './CarouselItem';
 
 import './Carousel.css';
@@ -6,18 +6,41 @@ import './Carousel.css';
 export default function Carousel({ children }) {
   const [activeIndex, setActiveIndex] = useState(0);
   console.log('ActiveIndex:', activeIndex);
+  const [pause, setPause] = useState(false);
+
   const updateIndex = (newIndex) => {
+    //infinite carousel, when you are in page 1, click prev, it goes back to page 3
     if (newIndex < 0) {
-      newIndex = 0;
-    } else if (newIndex >= React.Children.count(children)) {
       newIndex = React.Children.count(children) - 1;
+    }
+    //when you are in page 3 and click next, it goes to page 1 
+    else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
     };
-    console.log('index:', newIndex);
+    // console.log('index:', newIndex);
     setActiveIndex(newIndex);
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!pause) {
+        updateIndex(activeIndex + 1);
+      }
+    }, 2000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    }
+  })
+
   return (
-    <div className='carousel'>
+    <div
+      className='carousel'
+      onMouseEnter={() => { setPause(true) }}
+      onMouseLeave={() => { setPause(false) }}
+    >
       <div className='inner' style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
         {React.Children.map(children, (child, index) => {
           return React.cloneElement(child, { width: '100%' });
